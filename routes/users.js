69778fb600f0191d0aa447274fee033716e44e6d
Fouad <perceval@protonmail.com>
2021-11-16 22:56:52 +0100

    Post route created

--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,31 @@ import express from 'express';
 
 const router = express.Router();
 
+const users = [
+  {
+    firstName: "John",
+    lastName: "Wayne",
+    age: 54
+  },
+  {
+    firstName: "Jane",
+    lastName: "Tarzan",
+    age: 34
+  }
+]
+
 // all routes in here are starting with /users
 router.get('/', (req, res) => {
-  res.send('HELLO');
+  console.log(users);
+
+  res.send(users);
 });
 
+router.post('/', (req, res) => {
+  const user = req.body;
+  users.push(user);
+
+  res.send(`User with the name ${user.firstName} added to database!`);
+})
 
 export default router;
