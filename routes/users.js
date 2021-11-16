30956d7699da81c7a09d8e07b365704f263c21a6
Fouad <perceval@protonmail.com>
2021-11-16 23:47:07 +0100

    Add the delete route users

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
 
-const users = [];
+let users = [];
 
 // all routes in here are starting with /users
 router.get('/', (req, res) => {
@@ -18,6 +18,22 @@ router.post('/', (req, res) => {
   users.push({...user, id: uuidv4() });
 
   res.send(`User with the name ${user.firstName} added to database!`);
+})
+
+router.get('/:id', (req, res) => {
+  const {id} = req.params;
+
+  const foundUser = users.find((user) => user.id === id);
+
+  res.send(foundUser);
+})
+
+router.delete('/:id', (req, res) => {
+  const {id} = req.params;
+
+  users = users.filter((user) => user.id !== id);
+
+  res.send(`User with the id ${id} deleted from the database. `)
 })
 
 export default router;
