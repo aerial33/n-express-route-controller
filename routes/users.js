a2376ec4d489687e9d785820b445caec1304116e
Fouad <perceval@protonmail.com>
2021-11-17 00:35:09 +0100

    refacto with controller and routes folders

--- routes/users.js
+++ routes/users.js
@@ -1,52 +1,21 @@
 import express from 'express';
-import { v4 as uuidv4 } from 'uuid';
+
+import { createUser, getUsers, getUser, deleteUser, updateUser } from '../controllers/users.js';
+
 
 const router = express.Router();
 
-let users = [];
 
 // all routes in here are starting with /users
-router.get('/', (req, res) => {
-  console.log(users);
+router.get('/', getUsers);
 
-  res.send(users);
-});
 
-router.post('/', (req, res) => {
-  const user = req.body;
+router.post('/', createUser);
 
-  users.push({...user, id: uuidv4() });
+router.get('/:id', getUser)
 
-  res.send(`User with the name ${user.firstName} added to database!`);
-})
+router.delete('/:id', deleteUser)
 
-router.get('/:id', (req, res) => {
-  const {id} = req.params;
-
-  const foundUser = users.find((user) => user.id === id);
-
-  res.send(foundUser);
-})
-
-router.delete('/:id', (req, res) => {
-  const {id} = req.params;
-
-  users = users.filter((user) => user.id !== id);
-
-  res.send(`User with the id ${id} deleted from the database. `)
-})
-
-router.patch('/:id', (req, res) => {
-  const {id} = req.params;
-  const { firstName, lastName, age } = req.body;
-
-  const user = users.find((user) => user.id === id);
-
-  if(firstName) user.firstName = firstName;
-  if(lastName) user.lastName = lastName;
-  if(age) user.age = age;
-
-  res.send(`User with the id ${id} has been updated`);
-})
+router.patch('/:id', updateUser)
 
 export default router;
