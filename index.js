e61ba7a53fdefed7e8a0ed4405df13b76bc7d1aa
Fouad <perceval@protonmail.com>
2021-11-16 22:31:41 +0100

    first commit and first route users

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express'; 
+// we can use import in node because in package.json we have type: "module"
+import usersRoutes from './routes/users.js';
+
+const app = express();
+const PORT = 3000;
+
+
+app.use(express.json());
+
+app.use('/users', usersRoutes);
+
+app.get('/', (req, res) =>  res.send('Hello from Homepage.'));
+
+app.listen(PORT, () => console.log(`Server Running on port: http://localhost:${PORT}`));
